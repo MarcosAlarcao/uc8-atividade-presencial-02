2430aebe37627e3786bb8c18dd451389b62dfa6a
MarcosAlarcao <89554741+MarcosAlarcao@users.noreply.github.com>
2021-09-14 23:53:32 -0300

    Update Atividadeonline.js

--- Atividadeonline.js
+++ Atividadeonline.js
@@ -1,6 +1,22 @@
 var dataEvento=' 08/09/2021'
 var dataHoje= ' 14/09/2121'
 var idadePessoa= 16
-var listaParticipantes['participante1,'participante2']
+var listaParticipantes=['participante1,'participante2']
+var listaPalestrantes=['Palestrante1','Palestrante2']
+if (dataEvento<dataHoje){
+  console.log('o cadastro não será permitido por data inválida')
+}
 
-console.log 
+if (idadePessoa<18){
+  console.log('o cadastro não é permitido pela idade!')
+}
+for (let i=0;i<listaPalestrantes.length;i++){
+  console.log(listaPalestrantes[i])
+}
+for (let i=0;i<listaParticipantes.length;i++){
+  console.log(listaParticipantes[i])
+}
+if (listaParticipantes>100){
+  console.log('o cadastro não será permitido por ter excedido o limite')
+}
+
